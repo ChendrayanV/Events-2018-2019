2f81b574bb7d80c8ca4123c89b7bddc12714c204
Chendrayan Venkatesan <chendrayan.Venkatesan@pwc.com>
2018-08-30 08:34:02 -0400

    initial commit

--- SPSEvents-SEP2018/scripts/NewList.ps1
+++ SPSEvents-SEP2018/scripts/NewList.ps1
@@ -9,11 +9,11 @@ param(
     [ValidateSet('GenericList' , 'Announcements')]
     $ListTemplateType
 )
-$config = [xml](Get-Content .\config\config.xml)
+$config = [xml](.\config\config.xml)
 $admin = $config.config.snow.userName
 $password = $config.config.snow.password | ConvertTo-SecureString -AsPlainText -Force 
 $Credential = New-Object pscredential -ArgumentList ($admin, $password)
-$Uri = "https://dev42835.service-now.com/api/now/table/incident?sysparm_fields=number,state"
+$Uri = "https://dev42835.service-now.com/api/now/table/incident?sysparm_fields=number,state,sys_id"
 $Body = @{
     category            = "Software"
     "caller_id"         = $Caller_id
@@ -26,9 +26,7 @@ $Body = @{
     "assigned_to"       = "Chendrayan Venkatesan"
 } | ConvertTo-Json
 $IncidentResult = Invoke-RestMethod -Uri $Uri -Method Post -Body $Body -Credential $Credential -ContentType "application/json"
-
-Start-Sleep -Seconds 10 
-
+# Start-Sleep -Seconds 10 
 Import-Module 'C:\Projects\Events-2018-2019\SPSEvents-SEP2018\assemblies\Microsoft.SharePoint.Client.dll'
 $userName = $config.config.sharepoint.userName
 $adminPassword = $config.config.sharepoint.password | ConvertTo-SecureString -AsPlainText -Force
@@ -44,14 +42,26 @@ $ListCreationInformation.TemplateType = $ListTemplateType
 $Listc = $Web.Lists.Add($ListCreationInformation)
 $spoClientContext.ExecuteQuery()
 $spoClientContext.Dispose()
-
-Start-Sleep 45
-
+# Start-Sleep 15
 $List = $spoClientContext.Web.Lists.GetByTitle($ListName)
 $spoClientContext.Load($List)
 $spoClientContext.ExecuteQuery()
+$spoClientContext.Dispose()
+$Uri = "https://dev42835.service-now.com/api/now/table/incident/$($IncidentResult.result.sys_id)?sysparm_display_value=true&sysparm_fields=number,state,incident_state,sys_id"
+$Body = @{
+    work_notes = @{
+        ListName = $List.Title
+        Created = $List.Created
+        Notes = "Auto Closing the INC as per the requirement"
+        Contact = "chen@contoso.com"
+    }
+    state = "Resolved"
+} | ConvertTo-Json
+$IncidentInformation = Invoke-RestMethod -Uri $Uri -Method Patch -Body $Body -Credential $Credential
 [pscustomobject]@{
-    Incident_Number = $IncidentResult.result.Number
-    ListName        = $List.Title
-    Created         = $List.Created
+    Incident_Number = $IncidentInformation.result.Number
+    Incident_State = $IncidentInformation.result.state
+    Incident_Sys_Id = $IncidentInformation.result.sys_id
+    ListName = $List.Title
+    Created = $List.Created
 }
