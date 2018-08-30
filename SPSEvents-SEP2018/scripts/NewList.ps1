04f4b9f4a4522bdb5cde86c4c5b0588c816d2abb
Chendrayan Venkatesan <chendrayan.Venkatesan@pwc.com>
2018-08-30 06:59:03 -0400

    intial commit

--- SPSEvents-SEP2018/scripts/NewList.ps1
+++ SPSEvents-SEP2018/scripts/NewList.ps1
@@ -1,6 +1,23 @@
-Import-Module .\assemblies\Microsoft.SharePoint.Client.dll
-$userName = ""
-$adminPassword = "" | ConvertTo-SecureString -AsPlainText -Force
-$spoCredential = [Microsoft.SharePoint.Client.SharePointOnlineCredentials]::new($userName, $adminPassword)
-$spoClientContext = [Microsoft.SharePoint.Client.ClientContext]::new("https://chensoffice365-admin.sharepoint.com")
-$spoClientContext.Credentials = $spoCredential
+$admin = ""
+$password = "" | ConvertTo-SecureString -AsPlainText -Force 
+$Credential = New-Object pscredential -ArgumentList ($admin, $password)
+$Uri = "https://dev42835.service-now.com/api/now/table/incident?sysparm_fields=number,state"
+$Body = @{
+    category            = "Software"
+    "caller_id"         = "Chendrayan Venkatesan"
+    "contact_type"      = "Self-service"
+    impact              = "3 - Low"
+    urgency             = "3 - Low"
+    "short_description" = "Incident Record for Creating SharePoint List"
+    description         = "Incident Record for Creating SharePoint List - Requested By Chendrayan Venkatesan"
+    "assignment_group"  = "CAB Approval"
+    "assigned_to"       = "Chendrayan Venkatesan"
+} | ConvertTo-Json
+$IncidentResult = Invoke-RestMethod -Uri $Uri -Method Post -Body $Body -Credential $Credential -ContentType "application/json"
+[pscustomobject]@{
+    Incident_Number = $IncidentResult.result.number
+    State           = $IncidentResult.result.state 
+}
+
+Start-Sleep -Seconds 10 
+
